a3f184c01713e08028f9f17f1f9ea11679d4ac18
sirShovo <57877305+sirShovo@users.noreply.github.com>
2023-03-28 01:48:27 -0500

    Gets with params

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +8,20 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  // Recibiendo un parametro por get
+  @Get('product/:productId')
+  getProduct(@Param('productId') productId: number) {
+    return `Product ${productId}`;
+  }
+
+  // Recibiendo varios parametros por get
+  @Get('category/:categoryId/product/:productId')
+  getProducts(
+    @Param('categoryId') categoryId: number,
+    @Param('productId') productId: number,
+  ) {
+    return `Product ${productId} and the category is ${categoryId}`;
   }
 }
