ce05c36af2c358d4871fdb14c7238dc5b8820503
sirShovo <57877305+sirShovo@users.noreply.github.com>
2023-04-11 04:50:12 -0500

    configuraciones y uso de .envs

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,17 +1,23 @@
 import { Injectable, Inject } from '@nestjs/common';
 import { ConfigType } from '@nestjs/config';
 import config from './config';
+import { Db } from 'mongodb';
 
 @Injectable()
 export class AppService {
   constructor(
     // @Inject('API_KEY') private apiKey: string,
     @Inject('TASKS') private tasks: any[],
+    @Inject('MONGO') private database: Db,
     @Inject(config.KEY) private configService: ConfigType<typeof config>,
   ) {}
   getHello(): string {
     const apiKey = this.configService.apiKey;
     const name = this.configService.database.name;
     return `Hello World! ${apiKey} ${name}`;
+  }
+  getTasks() {
+    const taskCollection = this.database.collection('tasks');
+    return taskCollection.find().toArray();
   }
 }
--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,4 +1,7 @@
 import { Module, Global } from '@nestjs/common';
+import { ConfigType } from '@nestjs/config';
+import { MongoClient } from 'mongodb';
+import config from 'src/config';
 
 const API_KEY = '12345634';
 const API_KEY_PROD = 'PROD1212121SA';
@@ -10,7 +13,26 @@ const API_KEY_PROD = 'PROD1212121SA';
       provide: 'API_KEY',
       useValue: process.env.NODE_ENV === 'prod' ? API_KEY_PROD : API_KEY,
     },
+    {
+      provide: 'MONGO',
+      useFactory: async (configService: ConfigType<typeof config>) => {
+        const {
+          connection,
+          user,
+          password,
+          host,
+          port,
+          db,
+        } = configService.mongo;
+        const uri = `${connection}://${user}:${password}@${host}:${port}/?authMechanism=DEFAULT`;
+        const client = new MongoClient(uri);
+        await client.connect();
+        const database = client.db(db);
+        return database;
+      },
+      inject: [config.KEY],
+    },
   ],
-  exports: ['API_KEY'],
+  exports: ['API_KEY', 'MONGO'],
 })
 export class DatabaseModule {}
